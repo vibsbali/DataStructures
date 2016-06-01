3adaf88ff70cbbb75b4a9228a8f1d42e924d8802
Vaibhav Bali <vbali@internode.on.net>
2016-06-01 13:04:17 +1000

    Added a new type DoublyLinkedListNode

---
+++ Core/DoublyLinkedListNode.cs
@@ -0,0 +1,19 @@
+﻿namespace Core
+{
+    public class DoublyLinkedListNode<T>
+    {
+        public DoublyLinkedListNode<T> Next { get; set; }
+        public DoublyLinkedListNode<T> Previous { get; set; }
+
+        public T Value { get; private set; }
+        public DoublyLinkedListNode(T value)
+        {
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+}
