e4b051099d196c5668c06d133c802413cbe369c7
Vaibhav Bali <vbali@internode.on.net>
2016-06-01 13:22:57 +1000

    Implemented Contains and AddFirst methods

---
+++ ConcreteStructures/DoublyLinkedList.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Core;
+
+namespace ConcreteStructures
+{
+    public class DoublyLinkedList<T> : ICollection<T>
+    {
+        private DoublyLinkedListNode<T> Head { get; set; }
+        private DoublyLinkedListNode<T> Tail { get; set; } 
+
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var current = Head;
+
+            while (current != null)
+            {
+                yield return current.Value;
+
+                current.Previous = current;
+                current = current.Next;
+            }
+        }
+
+        public void AddFirst(T value)
+        {
+            var node = new DoublyLinkedListNode<T>(value);
+
+            //Save off the head
+            var temp = Head;
+
+            //Set Head to new node
+            Head = node;
+
+            //Set next pointer to temp
+            Head.Next = temp;
+
+            //If it is the only item in the list
+            if (Count == 0)
+            {
+                Tail = Head;
+            }
+            else
+            {
+                temp.Previous = Head;
+            }
+
+            Count++;
+        }
+
+        public void AddLast(T value)
+        {
+            //Invoke Add method
+            Add(value);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Add(T item)
+        {
+            
+        }
+
+        public void Clear()
+        {
+            Head = Tail = null;
+            Count = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            var current = Head;
+            while (current != null)
+            {
+                if (current.Value.Equals(item))
+                {
+                    return true;
+                }
+                current.Previous = current;
+                current = current.Next;
+            }
+
+            return false;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count { get; private set; }
+
+        public bool IsReadOnly => false;
+    }
+}
