e3808a77a1fefb9100d34c8055a1073203cf5b85
Vaibhav Bali <vibsbali@gmail.com>
2016-06-03 10:24:41 +1000

    Implemented functionality for Stack

--- ConcreteStructures/CircularLinkedList.cs
+++ ConcreteStructures/CircularLinkedList.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.Serialization.Formatters;
 using Core;
 
 namespace ConcreteStructures
---
+++ ConcreteStructures/DsStack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConcreteStructures
+{
+    public class DsStack<T> 
+    {
+        private LinkedList<T> backingLinkedList = new LinkedList<T>();
+
+        public void Push(T item)
+        {
+            backingLinkedList.AddLast(item);
+        }
+
+        public T Pop()
+        {
+            if (backingLinkedList.Count == 0)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+
+            var item = backingLinkedList.Last.Value;
+            backingLinkedList.RemoveLast();
+            return item;
+        }
+
+        public T Peek()
+        {
+            if (backingLinkedList.Count == 0)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+
+            return backingLinkedList.Last.Value;
+        }
+
+        public int Count()
+        {
+            return backingLinkedList.Count;
+        }
+    }
+}
