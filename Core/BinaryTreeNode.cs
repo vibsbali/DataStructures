7eff67057963f4d7dfd468908dce52073d019a1e
Vaibhav Bali <vibsbali@gmail.com>
2016-06-05 10:47:12 +1000

    Created a BinaryTreeNode

---
+++ Core/BinaryTreeNode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Core
+{
+    public class BinaryTreeNode<T> : IComparable<T> 
+                 where T : IComparable<T>
+    {
+        public T Value { get; }
+
+        public BinaryTreeNode<T> Right { get; set; }
+        public BinaryTreeNode<T> Left { get; set; }
+
+        public BinaryTreeNode(T value)
+        {
+            Value = value;
+        }
+
+        public int CompareTo(T other)
+        {
+            return Value.CompareTo(other);
+        }
+    }
+}
