3fbeb154c114008e4ae2323e2690a74f8db8d7b2
Vaibhav Bali <vibsbali@gmail.com>
2016-06-07 11:21:59 +1000

    Created a new Node type

---
+++ CoreNew/Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CoreNew
+{
+    public class Node<T> where T: IComparable<T>
+    {
+        public T Value { get; private set; }
+
+        public Node(T value)
+        {
+            Value = value;
+        }
+
+        public Node<T> Next { get; set; }
+    }
+}
