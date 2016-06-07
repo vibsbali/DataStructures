dca1b2c569a50dc471c7b39686961821ccb40549
Vaibhav Bali <vibsbali@gmail.com>
2016-06-07 12:33:02 +1000

    Passed Tests

--- DataStructuresTests/LinkedListTests.cs
+++ DataStructuresTests/LinkedListTests.cs
@@ -8,22 +8,63 @@ namespace DataStructuresTests
     public class LinkedListTests
     {
         private LinkedList<int> list;
-            
+        private Node<int> nodeOne;
+        private Node<int> nodeTwo;
+        private Node<int> nodeThree;
+        private Node<int> nodeFour;
+
         [TestInitialize]
         public void Startup()
         {
             list = new LinkedList<int>();   
+            nodeOne = new Node<int>(1);
+            nodeTwo = new Node<int>(2);
+            nodeThree = new Node<int>(3);
+            nodeFour = new Node<int>(4);
         }
 
 
         [TestMethod]
         public void CreateAListWithOneNodeAssertHeadEqualTailAndCountIsOne()
         {
-            list.AddToFront(new Node<int>(1));
+            list.AddToFront(nodeOne);
 
             Assert.AreEqual(list.Head.Value, 1);
             Assert.AreEqual(list.Head.Value, list.Tail.Value);
             Assert.IsTrue(list.Count == 1);
+        }
+
+        [TestMethod]
+        public void CreateAListWithTwoNodeAssertHeadIsNotEqualTailAndCountIsTwo()
+        {
+            list.AddToFront(nodeOne);
+            list.AddToFront(nodeTwo);
+
+            Assert.AreEqual(list.Head.Value, 2);
+            Assert.AreNotEqual(list.Head.Value, list.Tail.Value);
+            Assert.IsTrue(list.Count == 2);
+        }
+
+
+        [TestMethod]
+        public void CreateAListWithTwoNodeAndInsertNewNodeToHeadAssertHeadIsEqualToNewNode()
+        {
+            list.AddToFront(nodeOne);
+            list.AddToFront(nodeTwo);
+
+            Assert.AreEqual(list.Head.Value, 2);
+            Assert.AreEqual(list.Head, nodeTwo);
+            Assert.IsTrue(list.Count == 2);
+        }
+
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            list = null;
+            nodeOne = null;
+            nodeTwo = null;
+            nodeThree = null;
         }
     }
 }
