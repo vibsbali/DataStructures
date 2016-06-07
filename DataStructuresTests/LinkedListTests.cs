e94a70b2c13fec42b164b8177040fe8b7c78540c
Vaibhav Bali <vibsbali@gmail.com>
2016-06-07 12:03:49 +1000

    Created a Test Project

---
+++ DataStructuresTests/LinkedListTests.cs
@@ -0,0 +1,29 @@
+﻿using ConcreteStructuresTwo;
+using CoreNew;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataStructuresTests
+{
+    [TestClass]
+    public class LinkedListTests
+    {
+        private LinkedList<int> list;
+            
+        [TestInitialize]
+        public void Startup()
+        {
+            list = new LinkedList<int>();   
+        }
+
+
+        [TestMethod]
+        public void CreateAListWithOneNodeAssertHeadEqualTailAndCountIsOne()
+        {
+            list.AddToFront(new Node<int>(1));
+
+            Assert.AreEqual(list.Head.Value, 1);
+            Assert.AreEqual(list.Head.Value, list.Tail.Value);
+            Assert.IsTrue(list.Count == 1);
+        }
+    }
+}
