ed89f8023b4c7191edbda7553888a23c2d8e7943
Vaibhav Bali <vibsbali@gmail.com>
2016-06-07 11:39:54 +1000

    Created Node DataStructure

--- CoreNew/Node.cs
+++ CoreNew/Node.cs
@@ -4,13 +4,27 @@ namespace CoreNew
 {
     public class Node<T> where T: IComparable<T>
     {
+        private Node<T> next;    
         public T Value { get; private set; }
-
+      
         public Node(T value)
         {
             Value = value;
         }
 
-        public Node<T> Next { get; set; }
+        public Node<T> Next
+        {
+            get { return next; }
+
+            set
+            {
+                if (value == this)
+                {
+                    throw new InvalidOperationException("Cant point node to itself");
+                }
+
+                next = value;
+            }
+        }
     }
 }
