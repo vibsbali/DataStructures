14d186a59758ab0ae351d08a114c5fd7e59ae59d
Vaibhav Bali <vibsbali@gmail.com>
2016-06-07 13:53:42 +1000

    Created a new type DoublyNode to represent Doubly Link Node

---
+++ CoreNew/DoublyNode.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace CoreNew
+{
+    class DoublyNode<T>
+    {
+        private DoublyNode<T> next;
+        private DoublyNode<T> previous;
+         
+        public T Value { get; private set; }
+
+        public DoublyNode(T value)
+        {
+            Value = value;
+        }
+
+        public DoublyNode<T> Next
+        {
+            get { return next; }
+
+            set
+            {
+                if (value == this)
+                {
+                    throw new InvalidOperationException("Cant point DoublyNode to itself");
+                }
+
+                next = value;
+            }
+        }
+
+        public DoublyNode<T> Previous
+        {
+            get { return previous; }
+
+            set
+            {
+                if (value == this)
+                {
+                    throw new InvalidOperationException("Cant point DoublyNode to itself");
+                }
+
+                previous = value;
+            }
+        }
+    }
+}
