e6cfd4ade005136bae58cf4e74209cc7a8d082d0
Vaibhav Bali <vibsbali@gmail.com>
2016-06-08 18:57:17 +1000

    Added a new ArrayList test project

---
+++ DataStructuresTests/ArrayListTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataStructures;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataStructuresTests
+{
+    [TestClass]
+    public class ArrayListTests
+    {
+        private ArrayList<int> list;
+
+        [TestInitialize]
+        public void Startup()
+        {
+            list = new ArrayList<int>();
+        }
+
+
+        [TestMethod]
+        public void CreateAList_AssertCountZero_AddOneItem_AssertCountOne()
+        {
+            Assert.AreEqual(0, list.Count);
+
+            list.Add(1);
+            Assert.AreEqual(1, list.Count);
+        }
+
+        [TestMethod]
+        public void CreateAList_Insert5Items_AssertCountFive()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                list.Add(i);
+            }
+            Assert.AreEqual(5, list.Count);
+        }
+
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            list = null;
+        }
+    }
+}
