95e5e12169d023f1ba4e1a4b6df3535af89a8c28
Vaibhav Bali <vibsbali@gmail.com>
2016-06-08 18:49:47 +1000

    Added a new type ArrayList<T>

---
+++ ConcreteStructuresTwo/ArrayList.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DataStructures
+{
+    public class ArrayList<T> : IList<T>
+    {
+        private T[] backingArray;
+
+        public ArrayList(int length)
+        {
+            if (length < 0)
+            {
+                throw new ArgumentException("length");
+            }
+
+            backingArray = new T[length];
+        }
+
+        //Min length is four
+        public ArrayList() : this(4)
+        {
+        }
+
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Add(T item)
+        {
+            if (backingArray.Length == Count)
+            {
+                var temp = new T[backingArray.Length * 2];
+                backingArray.CopyTo(temp, 0);
+                backingArray = temp;
+            }
+            backingArray[Count] = item;
+            ++Count;
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count { get; private set; }
+
+        public bool IsReadOnly => false;
+
+        public int IndexOf(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int index, T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T this[int index]
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -8,25 +8,7 @@ namespace ConsoleApp
     {
         private static void Main()
         {
-            var list = new DoublyLinkedList<int>();
-            list.Add(1);
-            list.Add(2);
             
-            foreach (var i in list)
-            {
-                Console.WriteLine(i);
-            }
-
-
-            list.RemoveFromBack();
-            list.RemoveFromFront();
-
-            var current = list.Head;
-            while (current != null)
-            {
-                Console.WriteLine(current.Value);
-                current = current.Next;
-            }
             
         }
 
