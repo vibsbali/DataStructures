13187d776ef8589c5166e4a136b641dcf5923d0b
Vaibhav Bali <vibsbali@gmail.com>
2016-06-09 20:28:04 +1000

    Create a new type Deque

---
+++ ConcreteStructuresTwo/Deque.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DataStructures
+{
+    public class Deque<T>
+    {
+        LinkedList<T> items = new LinkedList<T>();
+
+        public void EnqueueFirst(T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EnqueueLast(T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T DequeueFirst()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T DequeueLast()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T PeekFirst()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T PeekLast()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count { get;  }
+    }
+}
