42e3b836e39bed58a6da79bee4411cc2b40e057f
Vaibhav Bali <vibsbali@gmail.com>
2016-06-09 20:08:48 +1000

    Completed Stack class

---
+++ ConcreteStructuresTwo/Stack.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DataStructures
+{
+    public class Stack<T>
+    {
+        readonly System.Collections.Generic.LinkedList<T> items = new System.Collections.Generic.LinkedList<T>();
+
+        public int Count { get; private set; }
+
+        public void Push(T item)
+        {
+            items.AddLast(item);
+            Count++;
+        }
+
+        public T Pop()
+        {
+            if (items.Count == 0)
+            {
+                throw new InvalidOperationException("The stack is empty");
+            }
+
+            var item = items.Last;
+            items.RemoveLast();
+            Count--;
+
+            return item.Value;
+        }
+
+        public T Peek()
+        {
+            return items.Last.Value;
+        }
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,42 +1,10 @@
-﻿using System;
-using DataStructures;
-
-namespace ConsoleApp
+﻿namespace ConsoleApp
 {
     class Program
     {
         private static void Main()
         {
-            var list = new DataStructures.ArrayList<int>();
-
-            for (int i = 0; i < 2; i++)
-            {
-                list.Add(i);
-            }
-
-
-            for (int i = 0; i < list.Count; i++)
-            {
-                Console.WriteLine(list[i]);
-            }
-
-
-            list.Insert(1, 2);
-
-            
-
-            for (int i = 0; i < list.Count; i++)
-            {
-                Console.WriteLine(list[i]);
-            }
-        }
-
-        static void Print<T>(T[] stack)
-        {
-            for (int i = 0; i < stack.Length; i++)
-            {
-                Console.WriteLine(stack[i]);
-            }
+           
         }
     }
 }
