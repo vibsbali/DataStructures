b7deaf84f1bab5667181a05dbcbf9eb58e9cc002
Vaibhav Bali <vibsbali@gmail.com>
2016-06-10 16:24:13 +1000

    Implemented Stack using Array

---
+++ ConcreteStructuresTwo/StackArray.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace DataStructures
+{
+    public class StackArray<T>
+    {
+        private T[] backingArray;
+        private const int DefaultSize = 16;
+        public int Count { get; private set; }
+
+
+        public StackArray() : this(DefaultSize)
+        {
+
+        }
+
+        public StackArray(int size)
+        {
+            backingArray = new T[size];
+        }
+
+
+        private void GrowArray()
+        {
+            var temp = new T[backingArray.Length * 2];
+            Array.Copy(temp, 0, backingArray, 0, backingArray.Length - 1);
+            backingArray = temp;
+        }
+
+        public void Push(T item)
+        {
+            if (backingArray.Length == Count)
+            {
+                GrowArray();
+            }
+
+            backingArray[Count] = item;
+            ++Count;
+        }
+
+        public T Pop()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("Cannot call Pop on empty stack");
+            }
+
+            var itemToReturn = backingArray[Count - 1];
+            Count--;
+            return itemToReturn;
+        }
+
+        public T Peek()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("Cannot call Peek on empty stack");
+            }
+
+            return backingArray[Count - 1]; 
+        }
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace ConsoleApp
+﻿using DataStructures;
+
+namespace ConsoleApp
 {
     class Program
     {
         private static void Main()
         {
-           
+           var myArrayList = new ArrayList<int>();
+            myArrayList.Add(3);
+            myArrayList.Add(3);
+
         }
     }
 }
