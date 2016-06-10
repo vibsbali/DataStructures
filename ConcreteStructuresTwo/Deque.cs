4b474a08b39447db4612d7e335973a5248307477
Vaibhav Bali <vibsbali@gmail.com>
2016-06-10 13:06:11 +1000

    Implemented Deque

--- ConcreteStructuresTwo/Deque.cs
+++ ConcreteStructuresTwo/Deque.cs
@@ -4,38 +4,70 @@ namespace DataStructures
 {
     public class Deque<T>
     {
-        LinkedList<T> items = new LinkedList<T>();
+        System.Collections.Generic.LinkedList<T> items = new System.Collections.Generic.LinkedList<T>();
 
+        //Complexity O(1) constant
         public void EnqueueFirst(T value)
         {
-            throw new NotImplementedException();
+            items.AddFirst(value);
         }
 
+        //Complexity O(1) constant
         public void EnqueueLast(T value)
         {
-            throw new NotImplementedException();
+            items.AddLast(value);
         }
 
+        //Complexity O(1) constant
         public T DequeueFirst()
         {
-            throw new NotImplementedException();
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("Cannot Dequeue on empty Deque");
+            }
+
+            var itemToReturn = items.First;
+            items.RemoveFirst();
+
+            return itemToReturn.Value;
         }
 
+        //Complexity O(1) constant
         public T DequeueLast()
         {
-            throw new NotImplementedException();
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("Cannot Dequeue on empty Deque");
+            }
+
+            var itemToReturn = items.Last;
+            items.RemoveLast();
+
+            return itemToReturn.Value;
         }
 
+        //Complexity O(1) constant
         public T PeekFirst()
         {
-            throw new NotImplementedException();
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("PeekFirst called when deque is empty");
+            }
+
+            return items.First.Value;
         }
 
+        //Complexity O(1) constant
         public T PeekLast()
         {
-            throw new NotImplementedException();
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("PeekLast called when deque is empty");
+            }
+
+            return items.Last.Value;
         }
 
-        public int Count { get;  }
+        public int Count => items.Count;
     }
 }
