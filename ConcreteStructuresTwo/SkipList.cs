828bb1d773bfd3752bc55ef116fb9b2dea9813bf
Vaibhav Bali <vibsbali@gmail.com>
2016-06-17 10:51:20 +1000

    Implemented Add functionality and Random functionality

--- ConcreteStructuresTwo/SkipList.cs
+++ ConcreteStructuresTwo/SkipList.cs
@@ -16,7 +16,6 @@ namespace DataStructures
         //There is always one level of depth(the base list)
         private int levels = 1;
 
-
         public IEnumerator<T> GetEnumerator()
         {
             throw new NotImplementedException();
@@ -29,20 +28,69 @@ namespace DataStructures
 
         public void Add(T item)
         {
-            int levels = PickRandomLevel();
+            int level = PickRandomLevel();
 
-            var newNode = new SkipListNode<T>(item, levels + 1);
+            var newNode = new SkipListNode<T>(item, level + 1);
             var current = head;
 
-            for (int i = levels; i >= 0; i--)
+            for (int i = level; i >= 0; i--)
             {
-                
+                while (current.Next[i] != null)
+                {
+                    //LHS > RHS
+                    if (current.Next[i].Value.CompareTo(item) > 0)
+                    {
+                        break;
+                    }
+
+                    current = current.Next[i];
+                }
+
+                if (i <= level)
+                {
+                    //Adding "c" to the list: a -> b -> d -> e
+                    //Current is node b and current.Next[i] is d
+
+                    //1. Link the new node (c) to existing node (d) :
+                    //c.Next = d
+                    newNode.Next[i] = current.Next[i];
+
+                    //Insert c into the list after b:
+                    //b.Next = c
+                    current.Next[i] = newNode;
+                }
             }
+
+            Count++;
         }
 
         private int PickRandomLevel()
         {
-            throw new NotImplementedException();
+            int random = rand.Next();
+            int level = 0;
+
+            // We're using the bit mask of a random integer to determine if the max
+            // level should increase by one or not.
+            // Say the 8 LSBs of the int are 00101100. In that case, when the
+            // LSB is compared against 1, it tests to 0 and the while loop is never
+            // entered so the level stays the same. That should happen 1/2 of the time.
+            // Later, if the _levels field is set to 3 and the rand value is 01101111,
+            // the while loop will run 4 times and on the last iteration will
+            // run another 4 times, creating a node with a skip list height of 4. This should
+            // only happen 1/16 of the time.
+            while ((random & 1) == 1)
+            {
+                if (level == levels)
+                {
+                    levels++;
+                    break;
+                }
+
+                random >>= 1;
+                level++;
+            }
+
+            return level;
         }
 
         public void Clear()
