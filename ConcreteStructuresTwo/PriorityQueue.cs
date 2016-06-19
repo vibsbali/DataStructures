94c112bc67dfea7653393b5c037143053fafead7
Vaibhav Bali <vibsbali@gmail.com>
2016-06-19 17:18:09 +1000

    Created a new class PriorityQueue

---
+++ ConcreteStructuresTwo/PriorityQueue.cs
@@ -0,0 +1,30 @@
+﻿/*
+    Priority Queue is a very thin wrapper over a heap
+*/
+
+using System;
+
+namespace DataStructures
+{
+    public class PriorityQueue<T> where T : IComparable<T>
+    {
+        Heap<T> heap = new Heap<T>();
+
+        public void Enqueue(T value)
+        {
+            heap.Add(value);
+        }
+
+        public T Deque()
+        {
+            return heap.RemoveMax();
+        }
+
+        public void Clear()
+        {
+            heap.Clear();
+        }
+
+        public int Count => heap.Count;
+    }
+}
