a5d3ba07c277ba367137c4e5f1d80d447fba5727
Vaibhav Bali <vibsbali@gmail.com>
2016-06-19 23:58:17 +1000

    Created new types for implementing AVL Tree

---
+++ ConcreteStructuresTwo/AvlTree.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using CoreNew;
+
+namespace DataStructures
+{
+    public class AvlTree<T> : IEnumerable<T>
+        where T : IComparable<T>
+    {
+        public AvlTreeNode<T> Head { get; private set; }
+
+        public void Add(T value)
+        {
+            if (Head == null)
+            {
+                Head = new AvlTreeNode<T>(value, null);
+            }
+            else
+            {
+                InsertNode(value, Head);
+            }
+            Count++;
+        }
+
+        private void InsertNode(T value, AvlTreeNode<T> parentNode)
+        {
+            //Is LHS > RHS - go left
+            if (parentNode.Value.CompareTo(value) > 0)
+            {
+                if (parentNode.Left == null)
+                {
+                    parentNode.Left = new AvlTreeNode<T>(value, parentNode);
+                    return;
+                }
+                InsertNode(value, parentNode.Left);
+            }
+            //Otherwise LHS <= RHS - go right
+            else
+            {
+                if (parentNode.Right == null)
+                {
+                    parentNode.Right = new AvlTreeNode<T>(value, parentNode);
+                    return;
+                }
+                InsertNode(value, parentNode.Right);
+            }
+        }
+
+        public bool Contains(T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            
+        }
+
+        public int Count { get; private set; }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ CoreNew/AVLTreeNode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreNew
+{
+    public class AvlTreeNode<T> 
+        where T : IComparable<T>
+    {
+        public AvlTreeNode<T> Parent { get; private set; }
+        public AvlTreeNode<T> Left { get; set; }
+        public AvlTreeNode<T> Right { get; set; }
+
+        public T Value { get; private set; }
+
+        public AvlTreeNode(T value, AvlTreeNode<T> parent)
+        {
+            Parent = parent;
+            Value = value;
+        }
+    }
+}
